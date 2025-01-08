3a6324b0b46f9ac0821a5bae6d87cf595b0786e0
schxzo8 <subhaghimire8@gmail.com>
2025-01-08 11:44:39 +0545

    first commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Shubha{
+    String name; 
+    int age;
+    float marks;
+}
+
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
